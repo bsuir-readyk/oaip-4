666206b10a8e494535f82edd40d7bb7f66a556a4
readyyyk <mr.sairexer@gmail.com>
2024-11-30 00:42:01 +0300

    0-index

--- solve.dpr
+++ solve.dpr
@@ -8,7 +8,7 @@ var i: Integer;
 begin
   Randomize;
   Write('Полученный массив: ');
-  i := 1;
+  i := 0;
   while i < length(arr) do
   begin
     arr[i] := random(100);
@@ -22,10 +22,10 @@ procedure FillManual(var arr: TIntegerArr);
 var i: Integer;
 begin
   Writeln('Введите элементы');
-  i := 1;
+  i := 0;
   while i < length(arr) do
   begin
-    Write(i, ': ');
+    Write(i + 1, ': ');
     Read(arr[i]);
     Inc(i);
   end;
@@ -43,39 +43,41 @@ begin
     ReadLn(n);
   end;
 
-  SetLength(arr, n + 1);
-  SetLength(dArr, n + 1);
-  SetLength(resArr, n + 1);
-  SetLength(pArr, n + 1);
-  SetLength(prevArr, n + 1);
+  SetLength(arr, n);
+  SetLength(dArr, n);
+  SetLength(resArr, n);
+  SetLength(pArr, n);
+  SetLength(prevArr, n);
 
   FillRand(arr);
   // FillManual(arr);
 
-  i := 2;
-  while i <= n do
+  i := 1;
+  while i < n do
   begin
     dArr[i] := INF;
     pArr[i] := -1;
     Inc(i);
   end;
 
-  dArr[1] := arr[1];
-  pArr[1] := 1;
-  prevArr[1] := -1;
+  dArr[0] := arr[0];
+  pArr[0] := 0;
+  prevArr[0] := -1;
 
-  i := 2;
-  while i <= n do
+  i := 1;
+  while i < n do
   begin
-    j := 1;
-    while (j <= n) and (arr[i] > dArr[j]) do
-      j := j + 1;
+    j := 0;
+    while (j < n) and (arr[i] > dArr[j]) do
+    begin
+      Inc(j);
+    end;
 
-    if (j = 1) or (arr[i] > dArr[j - 1]) then
+    if (j = 0) or (arr[i] > dArr[j - 1]) then
     begin
       dArr[j] := arr[i];
       pArr[j] := i;
-      if j > 1 then
+      if j > 0 then
         prevArr[i] := pArr[j - 1]
       else
         prevArr[i] := -1;
@@ -85,14 +87,14 @@ begin
 
   resLength := 0;
   lastIndex := 0;
-  i := n;
-  while i >= 1 do
+  i := n - 1;
+  while i >= 0 do
   begin
     if (dArr[i] <> INF) then
     begin
-      resLength := i;
+      resLength := i + 1;
       lastIndex := pArr[i];
-      i := 1;
+      i := 0;
     end;
     Dec(i);
   end;
@@ -100,22 +102,21 @@ begin
   WriteLn('Число вычёркиваний: ', n - resLength);
   Write('Полученная последовательность: ');
 
-  pos := 1;
+  pos := 0;
   i := lastIndex;
   while i <> -1 do
   begin
-     resArr[pos] := arr[i];
+    resArr[pos] := arr[i];
     pos := pos + 1;
     i := prevArr[i];
   end;
 
   i := pos - 1;
-  while i >= 1 do
+  while i >= 0 do
   begin
     Write(resArr[i], ' ');
     Dec(i);
   end;
 
 end.
-
 
